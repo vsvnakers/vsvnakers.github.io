5372d535326b3c3f1c7b813069c6c2f529bca610
vsvnakers <1151259111@qq.com>
2025-04-03 20:45:08 +0800

    "2025-04-03 20:45:07"

--- auto_push.py
+++ auto_push.py
@@ -1,15 +1,17 @@
 import subprocess
 from datetime import datetime
 
+GIT = r"C:\Program Files\Git\bin\git.exe"
+
 def run_git_commands():
     # 当前时间作为 commit 信息
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     commit_message = f'"{now}"'
 
     try:
-        subprocess.run(["git", "add", "."], check=True)
-        subprocess.run(["git", "commit", "-m", commit_message], check=True)
-        subprocess.run({"git", "push", "origin", "main"}, check=True)
+        subprocess.run([GIT, "add", "."], check=True)
+        subprocess.run([GIT, "commit", "-m", commit_message], check=True)
+        subprocess.run({GIT, "push", "origin", "main"}, check=True)
         print(f"✅ 已成功提交并推送：{commit_message}")
     except subprocess.CalledProcessError as e:
         print(f"❌ 出现错误：{e}")
