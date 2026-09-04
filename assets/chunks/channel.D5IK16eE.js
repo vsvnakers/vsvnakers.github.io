3c39a3241d1e606471387e82d19f30ddcdf77d67
vsvnakers <vsvnakers@users.noreply.github.com>
2026-09-04 15:05:18 +0000

    deploy: 9b46d6bd31b863d12d1d2cf56dec9c9d5c7d1970

--- assets/chunks/channel.D5IK16eE.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"../app.DeQHL4_o.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.Jkw-wB3r.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.DkswAlM9.js";import{_ as a}from"../app.CQ60lmC9.js";import"./chunk-5VM5RSS4.C4N-Nn1_.js";import"./chunk-XXDRQBXY.vSqaA8Up.js";import"./chunk-KBJHAD2P.BXO6QRsd.js";import"./chunk-2GRJ4B5K.BXe1hMsf.js";import"./channel.qsG8_vva.js";import"./framework.DV9MvnYb.js";import"./theme.Ckr3YTIe.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
