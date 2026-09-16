65f74c64631bb2fa4164593e6dc526537d9634ff
vsvnakers <vsvnakers@users.noreply.github.com>
2026-09-16 08:11:42 +0000

    deploy: d81bab51b26d28aba7a988433c11504654757c85

--- assets/chunks/swimlanesDiagram-ULZ7WXOC.2SigVCh3.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN.B8c1z1gc.js";import{_ as a}from"../app.CtSnU5Va.js";import"./chunk-5VM5RSS4.CfQmaNRv.js";import"./chunk-XXDRQBXY.UIlKTJFm.js";import"./chunk-KBJHAD2P.CyejbTZL.js";import"./chunk-2GRJ4B5K.BY9ZQxDa.js";import"./channel.Cv5YQ28v.js";import"./framework.ySNHTji0.js";import"./theme.Ds_1Fk7m.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
